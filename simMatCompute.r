6b2e08ef3839ec5e5e3c3a6fb0442fc3a3da9cfb
RidaJilani <50441346+RidaJilani@users.noreply.github.com>
2019-05-26 21:48:22 +0500

    Update simMatCompute.r

--- simMatCompute.r
+++ simMatCompute.r
@@ -1,26 +1,26 @@
+mydata <- data.frame(trial,stim1,stim2,response)
+
+mytable <- tapply(mydata$response,list(mydata$stim1,mydata$stim2),mean)
+
 cl1 <- cbind(rnorm(100,0.2,0.05),rnorm(100,0.8,0.06))
 cl2 <- cbind(rnorm(100,0.7,0.08),rnorm(100,0.3,0.05))
 x <- rbind(cl1,cl2)
 
-## create negative distance matrix (default Euclidean)
+## calculating eucledian distance
 simMat1 <- negDistMat(x)
 
-## compute similarities as squared negative distances
-## (in accordance with Frey's and Dueck's demos)
+## computing similiarity matrices.
 simMat2 <- negDistMat(x, r=2)
 
 ## compute RBF kernel
 simMat3 <- expSimMat(x, r=2)
 
-## compute similarities as squared negative distances
-## all samples versus a randomly chosen subset 
-## of 50 samples (for leveraged AP clustering)
+##
 sel <- sort(sample(1:nrow(x), nrow(x)*0.25)) 
 simMat4 <- negDistMat(x, sel, r=2)
 
 
-## example of leveraged AP using Minkowski distance with non-default
-## parameter p
+##
 cl1 <- cbind(rnorm(150,0.2,0.05),rnorm(150,0.8,0.06))
 cl2 <- cbind(rnorm(100,0.7,0.08),rnorm(100,0.3,0.05))
 x <- rbind(cl1,cl2)
